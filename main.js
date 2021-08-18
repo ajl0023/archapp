960319da4860c61912900f57e2e5c60de0776573
austin <austinlee199723@yahoo.com>
2021-08-17 22:03:38 -0700

    fixed scroll animations

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ const rightContainer = document.getElementById("right-container-scroll");
 let letScroll = true;
 
 const button = document.getElementById("test-button");
+const button2 = document.getElementById("test-button2");
+let windowHeight = window.innerHeight;
+let calcWindowHeight = 0;
+window.addEventListener("scroll", function () {});
 button.addEventListener("click", function () {
-  if (letScroll) {
-    letScroll = false;
-
-    leftContainer.style.transform = "translate3d(0px, -1240px, 0px)";
-    rightContainer.style.transform = "translate3d(0px, 1240px, 0px)";
-  }
+  calcWindowHeight += windowHeight;
+  leftContainer.style.transform = `translate3d(0px, -${calcWindowHeight}px, 0px)`;
+  rightContainer.style.transform = `translate3d(0px, ${calcWindowHeight}px, 0px)`;
+});
+button2.addEventListener("click", function () {
+    calcWindowHeight -= windowHeight;
+  rightContainer.style.transform = `translate3d(0px, -${calcWindowHeight}px, 0px)`;
+  leftContainer.style.transform = `translate3d(0px, ${calcWindowHeight}px, 0px)`;
 });
